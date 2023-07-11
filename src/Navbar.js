ecefee55c0ab0e4092b59fdfebd365b932f75207
Jovy Cortez <jovycortez11@gmail.com>
2023-07-11 15:47:18 -0400

    Dynamic height toggle for nav menu on smaller screen

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,20 +4,33 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
-  const [showLinks,setShowLinks] = useState(false);
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if(showLinks){
+      linksContainerRef.current.style.height = `${linksHeight}px`
+    } else {
+      linksContainerRef.current.style.height = '0px'
+    }
+  }, [showLinks])
+
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} alt="logo" />
-          <button 
-          className="nav-toggle"
-          onClick={()=> setShowLinks(!showLinks)}>
+          <button
+            className="nav-toggle"
+            onClick={() => setShowLinks(!showLinks)}>
             <FaBars />
           </button>
         </div>
-        <div className={`${showLinks ? 'links-container show-container' : 'links-container'}`}>
-          <ul className="links">
+
+        <div className='links-container' ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
             {links.map((link) => {
               const { id, url, text } = link;
               return (
@@ -30,10 +43,11 @@ const Navbar = () => {
             })}
           </ul>
         </div>
+
         <ul className="social-icons">
-          {social.map((item)=>{
-            const {id, url, icon} = item;
-            return(
+          {social.map((item) => {
+            const { id, url, icon } = item;
+            return (
               <li key={id}>
                 <a href={url}>
                   {icon}
